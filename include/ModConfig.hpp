15c36ad8cbc9d66750233c503074271b6afce3e1
kodenamekrak <kyelodge2005@gmail.com>
2025-04-26 16:37:37 +0100

    Update for 1.40.4

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -3,7 +3,7 @@
 #include "config-utils/shared/config-utils.hpp"
 
 
-DECLARE_CONFIG(ModConfig,
+DECLARE_CONFIG(ModConfig) {
     CONFIG_VALUE(Enabled, bool, "Enabled", true);
     CONFIG_VALUE(ColoredLasers, bool, "Colored lasers", true);
-)
+};
--- include/SettingsViewController.hpp
+++ include/SettingsViewController.hpp
@@ -1,7 +1,9 @@
 #pragma once
+
 #include "custom-types/shared/macros.hpp"
+
 #include "HMUI/ViewController.hpp"
 
-DECLARE_CLASS_CODEGEN(QonsistentSaberColors, SettingsViewController, HMUI::ViewController,
-DECLARE_OVERRIDE_METHOD(void, DidActivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
-)
+DECLARE_CLASS_CODEGEN(QonsistentSaberColors, SettingsViewController, HMUI::ViewController) {
+    DECLARE_OVERRIDE_METHOD_MATCH(void, DidActivate, &HMUI::ViewController::DidActivate, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+};
